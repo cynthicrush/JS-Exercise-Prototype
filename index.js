1e95037751575f218502a4c7d7ff99b01955d348
cynthicrush <cynthiazcanada@gmail.com>
2021-01-27 17:26:24 -0500

    Homework changes

--- index.js
+++ index.js
@@ -39,10 +39,23 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
+  Person.prototype.eat = function(edible) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
+  }
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  }
+  Person.prototype.toString = function() {
+    return this.name + ', ' + this.age;
+  }
+const person = new Person('Mary', 50); 
  
 
   
@@ -63,10 +76,24 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
-  
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
+  }
+  Car.prototype.drive = function(distance) {
+    this.odometer += distance;
+    this.tank -= ( distance / this.milesPerGallon );
+
+    if (this.tank === 0) {
+      return 'I ran out of fuel at ' + this.odometer + ' !'
+    }
+  }
+  const car = new Car('Jeep', 25);
   
   /*
     TASK 3
@@ -75,18 +102,22 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+  function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    this.favoriteToy = favoriteToy;
   }
- 
-  
+  Baby.prototype = Object.create(Person.prototype);
+  Baby.prototype.play = function() {
+    return 'Playing with ' + this.favoriteToy;
+  } 
+  const baby = new Baby('Alice', '7months', 'Barbie doll');
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. this represents a object in a called fuction. 
+    2. Implicit Binding is when we calling a function and there already had a object called it.
+    3. Explicit Binding is we used call, apply and bind methods to change this.
+    4. New Binding is when we made a new function.
   */
   
   
